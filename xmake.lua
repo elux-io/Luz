41a05269dafca7e18a71c4f8153fff67e6a4cddb
elux-io <133917886+elux-io@users.noreply.github.com>
2024-10-28 15:29:20 +0100

    ptn msvc respecte le standard wesh

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,8 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 set_warnings("allextra")
 
+add_cxxflags("/Zc:preprocessor", "/Zc:templateScope", "/Zc:throwingNew", "/Zc:enumTypes", { tools = "cl" })
+
 add_includedirs("src")
 
 target("main")
