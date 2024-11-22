39cb099efe73e1401cc62eff9f6ebe92ddc667a5
elux-io <133917886+elux-io@users.noreply.github.com>
2024-11-22 11:22:06 +0100

    DO_NOT_PAY_ATTENTION_TO_WHATEVER_THE_HELL_THE_COMPILER_IS_SAYING__THIS_ERROR_IS_CAUSED_BY_A_FAILED_ASSERT

--- src/Lux/Core/Debug.hpp
+++ src/Lux/Core/Debug.hpp
@@ -2,6 +2,8 @@
 
 #include "PlatformMacros.hpp"
 
+#include <Lux/Log/Log.hpp>
+
 #if LUX_COMPILER(MSVC)
 	#define LUX_DEBUG_BREAK() (__debugbreak())
 #elif LUX_COMPILER(CLANG) or LUX_COMPILER(GCC)
@@ -11,15 +13,23 @@
 #endif
 
 #if not defined(NDEBUG)
-	#define LUX_ASSERT(expr, ...) ((expr) or (LUX_DEBUG_BREAK(), false))
+	#define LUX_ASSERT(expr, ...) ((expr) or (::lux::detail::DO_NOT_PAY_ATTENTION_TO_WHATEVER_THE_HELL_THE_COMPILER_IS_SAYING__THIS_ERROR_IS_CAUSED_BY_A_FAILED_ASSERT(), LUX_LOG_ERROR("assertion `" #expr "` failed" __VA_OPT__(": ") __VA_ARGS__), LUX_DEBUG_BREAK(), false))
 #else
 	#define LUX_ASSERT(...) ((void)0)
 #endif
 
 namespace lux
 {
+	namespace detail
+	{
+		inline auto DO_NOT_PAY_ATTENTION_TO_WHATEVER_THE_HELL_THE_COMPILER_IS_SAYING__THIS_ERROR_IS_CAUSED_BY_A_FAILED_ASSERT() {}
+	}
+
 	[[noreturn]] inline auto Unreachable() -> void
 	{
+		LUX_LOG_ERROR("unreachable code reached");
+		LUX_DEBUG_BREAK();
+
 		#if LUX_COMPILER(MSVC)
 			__assume(false);
 		#elif LUX_COMPILER(CLANG) or LUX_COMPILER(GCC)
