115b98aaee0d62afde854105b36912337bb5f0be
elux-io <133917886+elux-io@users.noreply.github.com>
2024-12-12 23:38:42 +0100

    angle

---
+++ src/Lux/Math/Angle.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "Numbers.hpp"
+
+#include <concepts>
+#include <array>
+#include <cmath>
+#include <compare>
+
+namespace lux
+{
+	enum class AngularUnit
+	{
+		Turn,
+		Radian,
+		Degree,
+		Gon,
+	};
+
+	namespace detail
+	{
+		template<typename A>
+		struct IntoDeducer
+		{
+			A angle;
+
+			template<typename To> constexpr operator To() const { return To(angle); }
+		};
+
+		template<std::floating_point T>
+		constexpr auto ConversionFactors = std::array {
+		               // from \ to   Turn               Radian               Degree              Gon
+			std::array /* Turn */   { T(1),              T(num::Tau),         T(360),             T(400) },
+			std::array /* Radian */ { T(1.l / num::Tau), T(1),                T(180.l / num::Pi), T(400.l / num::Tau) },
+			std::array /* Degree */ { T(1.l / 360.l),    T(num::Pi / 180.l),  T(1),               T(400.l / 360.l) },
+			std::array /* Gon */    { T(1.l / 400.l),    T(num::Tau / 400.l), T(360.l / 400.l),   T(1) },
+		};
+
+		template<AngularUnit from, AngularUnit to, std::floating_point T>
+		constexpr auto Convert(T angle) -> T
+		{
+			if constexpr (from == to)
+				return angle;
+			else
+				return angle * ConversionFactors<T>[static_cast<std::size_t>(from)][static_cast<std::size_t>(to)];
+		}
+	}
+
+	template<AngularUnit unit, std::floating_point T>
+	class Angle
+	{
+	public:
+		constexpr Angle() = default;
+		constexpr Angle(T value) : m_value(value) {}
+		template<AngularUnit fromUnit, std::floating_point T2> constexpr explicit Angle(Angle<fromUnit, T2> angle) : m_value(detail::Convert<fromUnit, unit>(static_cast<T>(angle.Get()))) {}
+
+		// TODO: constexpr + optimize for turns??
+		auto Sin() const { return std::sin(ToRadians().Get()); }
+		auto Cos() const { return std::cos(ToRadians().Get()); }
+		auto Tan() const { return std::tan(ToRadians().Get()); }
+
+		constexpr auto Into() const { return detail::IntoDeducer(*this); }
+		constexpr auto ToTurns() const { return Angle<AngularUnit::Turn, T>(*this); }
+		constexpr auto ToDegrees() const { return Angle<AngularUnit::Degree, T>(*this); }
+		constexpr auto ToRadians() const { return Angle<AngularUnit::Radian, T>(*this); }
+		constexpr auto ToGons() const { return Angle<AngularUnit::Gon, T>(*this); }
+		constexpr auto Get() const { return m_value; }
+
+		constexpr auto& operator+=(Angle b) { m_value += b.m_value; return *this; }
+		constexpr auto& operator-=(Angle b) { m_value -= b.m_value; return *this; }
+		constexpr auto& operator*=(Angle b) { m_value *= b.m_value; return *this; }
+		constexpr auto& operator/=(Angle b) { m_value /= b.m_value; return *this; }
+
+		friend constexpr auto operator<=>(Angle a, Angle b) = default;
+
+		friend constexpr auto operator-(Angle a) { return Angle(-a.m_value); }
+
+		friend constexpr auto operator+(Angle a, Angle b) { return a += b; }
+		friend constexpr auto operator-(Angle a, Angle b) { return a -= b; }
+		friend constexpr auto operator*(Angle a, Angle b) { return a *= b; }
+		friend constexpr auto operator/(Angle a, Angle b) { return a /= b; }
+
+	private:
+		T m_value;
+	};
+
+	using Turns = Angle<AngularUnit::Turn, float>;
+	using Degrees = Angle<AngularUnit::Degree, float>;
+	using Radians = Angle<AngularUnit::Radian, float>;
+	using Gons = Angle<AngularUnit::Gon, float>;
+
+	namespace literals
+	{
+		consteval auto operator""_turn(long double value) { return Turns(static_cast<float>(value)); }
+		consteval auto operator""_deg(long double value) { return Degrees(static_cast<float>(value)); }
+		consteval auto operator""_rad(long double value) { return Radians(static_cast<float>(value)); }
+		consteval auto operator""_gon(long double value) { return Gons(static_cast<float>(value)); }
+	}
+}
---
+++ tests/Math/AngleTest.cpp
@@ -0,0 +1,226 @@
+#include <Lux/Math/Angle.hpp>
+#include <Lux/Math/Numbers.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <type_traits>
+
+using Catch::Matchers::WithinRel;
+using Catch::Matchers::WithinAbs;
+
+TEST_CASE("Angle", "[Math]")
+{
+	SECTION("ctor")
+	{
+		CHECK(lux::Angle<lux::AngularUnit::Turn, float>().Get() == 0.f);
+		CHECK(lux::Angle<lux::AngularUnit::Turn, float>(1.f).Get() == 1.f);
+		CHECK(lux::Angle<lux::AngularUnit::Degree, float>(1.f).Get() == 1.f);
+
+		auto const a = lux::Angle<lux::AngularUnit::Degree, double>();
+		auto const b = lux::Angle<lux::AngularUnit::Turn, float>(a);
+		CHECK(b.Get() == 0.f);
+
+		// implicit construction from T
+		STATIC_CHECK(std::is_convertible_v<float, lux::Angle<lux::AngularUnit::Turn, float>>);
+
+		// explicit construction from Angle
+		STATIC_CHECK_FALSE(std::is_convertible_v<lux::Angle<lux::AngularUnit::Degree, float>, lux::Angle<lux::AngularUnit::Turn, float>>);
+	}
+
+	SECTION("Into")
+	{
+		auto const a = lux::Angle<lux::AngularUnit::Radian, float>(lux::num::Tau);
+		lux::Angle<lux::AngularUnit::Degree, float> const b = a.Into();
+		CHECK_THAT(b.Get(), WithinRel(360.f));
+	}
+
+	SECTION("trigonometric functions")
+	{
+		using A = lux::Angle<lux::AngularUnit::Turn, float>;
+
+		CHECK_THAT(A(1.f).Sin(), WithinAbs(0.f, 1e-6f));
+		CHECK_THAT(A(1.f).Cos(), WithinRel(1.f));
+		CHECK_THAT(A(1.f).Tan(), WithinAbs(0.f, 1e-6f));
+	}
+
+	SECTION("conversions")
+	{
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			CHECK(a.ToTurns().Get() == 1.f);
+			CHECK_THAT(a.ToRadians().Get(), WithinRel(float(lux::num::Tau)));
+			CHECK_THAT(a.ToDegrees().Get(), WithinRel(360.f));
+			CHECK_THAT(a.ToGons().Get(), WithinRel(400.f));
+		}
+
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Radian, float>(lux::num::Tau);
+			CHECK_THAT(a.ToTurns().Get(), WithinRel(1.f));
+			CHECK(a.ToRadians().Get() == lux::num::Tau);
+			CHECK_THAT(a.ToDegrees().Get(), WithinRel(360.f));
+			CHECK_THAT(a.ToGons().Get(), WithinRel(400.f));
+		}
+
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Degree, float>(360.f);
+			CHECK_THAT(a.ToTurns().Get(), WithinRel(1.f));
+			CHECK_THAT(a.ToRadians().Get(), WithinRel(float(lux::num::Tau)));
+			CHECK(a.ToDegrees().Get() == 360.f);
+			CHECK_THAT(a.ToGons().Get(), WithinRel(400.f));
+		}
+
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Gon, float>(400.f);
+			CHECK_THAT(a.ToTurns().Get(), WithinRel(1.f));
+			CHECK_THAT(a.ToRadians().Get(), WithinRel(float(lux::num::Tau)));
+			CHECK_THAT(a.ToDegrees().Get(), WithinRel(360.f));
+			CHECK(a.ToGons().Get() == 400.f);
+		}
+	}
+
+	SECTION("operators")
+	{
+		// +=
+		{
+			auto a = lux::Angle<lux::AngularUnit::Turn, float>();
+			a += lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			CHECK(a.Get() == 1.f);
+		}
+
+		// -=
+		{
+			auto a = lux::Angle<lux::AngularUnit::Turn, float>(2.f);
+			a -= lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			CHECK(a.Get() == 1.f);
+		}
+
+		// *=
+		{
+			auto a = lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			a *= lux::Angle<lux::AngularUnit::Turn, float>(2.f);
+			CHECK(a.Get() == 2.f);
+		}
+
+		// /=
+		{
+			auto a = lux::Angle<lux::AngularUnit::Turn, float>(4.f);
+			a /= lux::Angle<lux::AngularUnit::Turn, float>(2.f);
+			CHECK(a.Get() == 2.f);
+		}
+
+		// <=>
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			auto const b = lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			CHECK(a <=> b == std::partial_ordering::equivalent);
+			CHECK(a == b);
+			CHECK_FALSE(a != b);
+			CHECK_FALSE(a < b);
+			CHECK_FALSE(a > b);
+			CHECK(a <= b);
+			CHECK(a >= b);
+		}
+
+		// +
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Turn, float>();
+			auto const b = lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			CHECK((a + b).Get() == 1.f);
+		}
+
+		// -
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Turn, float>();
+			auto const b = lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			CHECK((-b).Get() == -1.f);
+			CHECK((a - b).Get() == -1.f);
+		}
+
+		// *
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Turn, float>(1.f);
+			auto const b = lux::Angle<lux::AngularUnit::Turn, float>(2.f);
+			CHECK((a * b).Get() == 2.f);
+		}
+
+		// /
+		{
+			auto const a = lux::Angle<lux::AngularUnit::Turn, float>(8.f);
+			auto const b = lux::Angle<lux::AngularUnit::Turn, float>(2.f);
+			CHECK((a / b).Get() == 4.f);
+		}
+	}
+
+	SECTION("les opérateurs marchent avec un Angle ou un T à gauche ou à droite")
+	{
+		using A = lux::Angle<lux::AngularUnit::Turn, float>;
+
+		CHECK((A(1.f) += 1.f).Get() == 2.f);
+		CHECK((A(1.f) -= 1.f).Get() == 0.f);
+		CHECK((A(1.f) *= 2.f).Get() == 2.f);
+		CHECK((A(2.f) /= 1.f).Get() == 2.f);
+
+		CHECK(A(1.f) <=> 1.f == std::partial_ordering::equivalent);
+		CHECK(1.f <=> A(1.f) == std::partial_ordering::equivalent);
+
+		CHECK((A(1.f) + 1.f).Get() == 2.f);
+		CHECK((1.f + A(1.f)).Get() == 2.f);
+
+		CHECK((A(1.f) - 1.f).Get() == 0.f);
+		CHECK((1.f - A(1.f)).Get() == 0.f);
+
+		CHECK((A(1.f) * 2.f).Get() == 2.f);
+		CHECK((1.f * A(2.f)).Get() == 2.f);
+
+		CHECK((A(2.f) / 1.f).Get() == 2.f);
+		CHECK((2.f / A(1.f)).Get() == 2.f);
+	}
+
+	SECTION("aliases")
+	{
+		STATIC_CHECK(std::is_same_v<lux::Turns, lux::Angle<lux::AngularUnit::Turn, float>>);
+		STATIC_CHECK(std::is_same_v<lux::Degrees, lux::Angle<lux::AngularUnit::Degree, float>>);
+		STATIC_CHECK(std::is_same_v<lux::Radians, lux::Angle<lux::AngularUnit::Radian, float>>);
+		STATIC_CHECK(std::is_same_v<lux::Gons, lux::Angle<lux::AngularUnit::Gon, float>>);
+	}
+
+	SECTION("literals")
+	{
+		using namespace lux::literals;
+
+		STATIC_CHECK(std::is_same_v<decltype(1.0_turn), lux::Angle<lux::AngularUnit::Turn, float>>);
+		STATIC_CHECK(std::is_same_v<decltype(1.0_deg), lux::Angle<lux::AngularUnit::Degree, float>>);
+		STATIC_CHECK(std::is_same_v<decltype(1.0_rad), lux::Angle<lux::AngularUnit::Radian, float>>);
+		STATIC_CHECK(std::is_same_v<decltype(1.0_gon), lux::Angle<lux::AngularUnit::Gon, float>>);
+	}
+
+	SECTION("constexpr")
+	{
+		using A = lux::Angle<lux::AngularUnit::Turn, float>;
+		using B = lux::Angle<lux::AngularUnit::Degree, float>;
+
+		STATIC_CHECK(A().Get() == 0.f);
+		STATIC_CHECK(A(1.f).Get() == 1.f);
+		STATIC_CHECK(A(B()) == 0.f);
+
+		STATIC_CHECK(A().Into().operator B().Get() == 0.f);
+		STATIC_CHECK(A().ToTurns().Get() == 0.f);
+		STATIC_CHECK(A().ToDegrees().Get() == 0.f);
+		STATIC_CHECK(A().ToRadians().Get() == 0.f);
+		STATIC_CHECK(A().ToGons().Get() == 0.f);
+
+		[]() consteval {
+			auto a = A();
+			a += 0.f;
+			a -= 0.f;
+			a *= 0.f;
+			a /= 1.f;
+		}();
+
+		STATIC_CHECK(A() <=> A() == std::partial_ordering::equivalent);
+		STATIC_CHECK((-A()).Get() == 0.f);
+		STATIC_CHECK((A() + A()).Get() == 0.f);
+		STATIC_CHECK((A() - A()).Get() == 0.f);
+		STATIC_CHECK((A() * A()).Get() == 0.f);
+		STATIC_CHECK((A() / A(1.f)).Get() == 0.f);
+	}
+}
