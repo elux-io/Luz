22e91e43d3ff255479a2df806e81bae665829e5e
elux-io <133917886+elux-io@users.noreply.github.com>
2024-12-17 13:45:41 +0100

    rename examples to demos
    c'est trop long examples (:

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 set_languages("c++20")
 set_warnings("allextra")
 
-option("enable-examples", { default = false })
+option("enable-demos", { default = false })
 option("enable-tests", { default = false })
 
 add_rules("mode.debug", "mode.release", "mode.coverage")
@@ -12,8 +12,8 @@ target("lux")
 	add_includedirs("src", { public = true })
 	add_files("src/Lux/**.cpp")
 
-if has_config("enable-examples") then
-	includes("examples/*")
+if has_config("enable-demos") then
+	includes("demos/*")
 end
 
 if has_config("enable-tests") then
